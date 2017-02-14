dee856fdb51b644f02e2d9881052ba017bdbe9a9
Chad Martin <Chad Martin>
2017-02-13 21:27:45 -0800

    v7.32

--- api/movies.js
+++ api/movies.js
@@ -1,26 +1,25 @@
-"use strict";
-const express = require("express");
-const mongodb = require("mongodb");
-const db_1 = require("../data/db");
-let router = express.Router();
+var express = require("express");
+var mongodb = require("mongodb");
+var db_1 = require("../data/db");
+var router = express.Router();
 // ADD OR EDIT MOVIE
-router.post('/', (req, res) => {
-    let movie = req.body;
+router.post('/', function (req, res) {
+    var movie = req.body;
     movie._id = new mongodb.ObjectID(req.body._id);
-    db_1.default.db.collection('movies').save(req.body).then((newMovie) => {
+    db_1.default.db.collection('movies').save(req.body).then(function (newMovie) {
         res.json(newMovie);
     });
 });
 // GET MOVIES
-router.get('/', (req, res) => {
-    db_1.default.db.collection('movies').find().toArray().then((movies) => {
+router.get('/', function (req, res) {
+    db_1.default.db.collection('movies').find().toArray().then(function (movies) {
         res.json(movies);
     });
 });
 // DELETE MOVIE
-router.delete('/:id', (req, res) => {
-    let movieId = new mongodb.ObjectID(req.params['id']);
-    db_1.default.db.collection('movies').remove({ _id: movieId }).then(() => {
+router.delete('/:id', function (req, res) {
+    var movieId = new mongodb.ObjectID(req.params['id']);
+    db_1.default.db.collection('movies').remove({ _id: movieId }).then(function () {
         res.sendStatus(200);
     });
 });
